ac5f5856bd0a43368a1ad3c6e335b8c62f14a47b
Iman Baghery <Iman_baghery@yahoo.com>
2022-02-01 16:35:05 +0330

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import {lorem} from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'generatePassword';
+  title = lorem.sentence();
+ 
+  posts=[{
+    user:'mamad',
+    url:'url1'
+  },{
+    user:'iman',
+    url:'url2'
+  }];
 }
